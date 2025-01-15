100ded95498a37d71e439e4b2ce7307778d7c86d
Rishi <r29.makadia@gmail.com>
2025-01-16 01:36:14 +0530

    task

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Use react-router-dom
+
+import Task from "./components/new/feature"; // Correct import
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/task/:id" element={<Task />} /> {/* Dynamic Route */}
     </Routes>
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
